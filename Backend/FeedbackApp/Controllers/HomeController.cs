0f1d34e45c869f04ade4646c8cf5af4b3b8d42ff
Prasad Bhalerao <pbhalerao@xpanxion.com>
2019-03-19 14:59:00 +0530

    framework setup

---
+++ Backend/FeedbackApp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeedbackApp.Controllers
+{
+    [Route("api/hello")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Hello Angular4 and ASP.NET Core from Windows 10");
+        }
+    }
+}
---
+++ Backend/FeedbackApp/wwwroot/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="inline.bundle.js" />
+/// <reference path="main.bundle.js" />
+/// <reference path="polyfills.bundle.js" />
+/// <reference path="styles.bundle.js" />
+/// <reference path="vendor.bundle.js" />

15ce84f319740141bc9bf42819081f204c166d87
Prasad Bhalerao <pbhalerao@xpanxion.com>
2019-04-05 10:23:25 +0530

    cleaned repo

--- Backend/FeedbackApp/wwwroot/_references.js
+++
@@ -1,6 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="inline.bundle.js" />
-/// <reference path="main.bundle.js" />
-/// <reference path="polyfills.bundle.js" />
-/// <reference path="styles.bundle.js" />
-/// <reference path="vendor.bundle.js" />
